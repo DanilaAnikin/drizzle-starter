b998af7191a2a62e3e52e0e7ed4153952392af62
Danila Anikin <danila.s.anikin@gmail.com>
2025-02-19 13:17:12 +0100

    .

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,16 @@
-import { Body, Controller, DefaultValuePipe, Delete, Get, Param, ParseIntPipe, Post, Put, Query, Res, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  DefaultValuePipe,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  Query,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -9,58 +21,50 @@ import { Response } from 'express';
 
 @Controller('user')
 export class UserController {
-    constructor(private userService: UserService) {}
-    @Get()
-    @UseGuards(AuthGuard)
-    findAll(
-        @Query('limit', new DefaultValuePipe(50), ParseIntPipe) limit: number,
-        @Query('page', new DefaultValuePipe(0), ParseIntPipe) page: number,
-        @Query('order_by', new DefaultValuePipe('id')) orderBy: string,
-        @Query('order', new DefaultValuePipe('asc')) order: string,
-        @UserId() userId: number
-    ) {
-        return this.userService.findAll(limit, page, orderBy, order);
-    }
+  constructor(private userService: UserService) {}
+  @Get()
+  @UseGuards(AuthGuard)
+  findAll(
+    @Query('limit', new DefaultValuePipe(50), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(0), ParseIntPipe) page: number,
+    @Query('order_by', new DefaultValuePipe('id')) orderBy: string,
+    @Query('order', new DefaultValuePipe('asc')) order: string,
+  ) {
+    return this.userService.findAll(limit, page, orderBy, order);
+  }
 
-    @Get(':id')
-    findOne(
-        @Param('id', ParseIntPipe) id: number,
-    ) {
-        return this.userService.findOne(id);
-    }
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.findOne(id);
+  }
 
-    @Get('get-logged-user')
-    @UseGuards(AuthGuard)
-    getUserFromToken(
-        @Res({ passthrough: true }) res: Response,
-    ) {
-        return this.userService.getUserFromToken(res);
-    }
+  @Get('get-logged-user')
+  @UseGuards(AuthGuard)
+  getUserFromToken(@Res({ passthrough: true }) res: Response) {
+    const token = res.getHeader('Authorization') as string;
+    return this.userService.getUserFromToken(token.split(' ')[1]);
+  }
 
-    @Post('register')
-    createUser(
-        @Body() body: CreateUserDto
-    ) {
-        return this.userService.createUser(body);
-    }
+  @Post('register')
+  createUser(@Body() body: CreateUserDto) {
+    return this.userService.createUser(body);
+  }
 
-    @Put(':id')
-    @UseGuards(AuthGuard)
-    updateUser(
-        @Body() body: UpdateUserDto,
-        @UserId() userId: number
-    ) {
-        return this.userService.updateUser(userId, body);
-    }
+  @Put(':id')
+  @UseGuards(AuthGuard)
+  updateUser(@Body() body: UpdateUserDto, @UserId() userId: number) {
+    return this.userService.updateUser(userId, body);
+  }
 
-    @Post('login')
-    async loginUser(
-        @Body() body: LoginUserDto,
-        @Res({ passthrough: true }) res: Response,
-    ) {
-        const token = await this.userService.loginUser(body);
-        res.setHeader('Authorization', `Bearer ${token}`);
-        
-        return token;
-    }
+  @Post('login')
+  async loginUser(
+    @Body() body: LoginUserDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    console.log(body);
+    const token = await this.userService.loginUser(body);
+    res.setHeader('Authorization', `Bearer ${token}`);
+
+    return token;
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,11 @@
-import { BadRequestException, ConflictException, Inject, Injectable, InternalServerErrorException, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Inject,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { DRIZZLE } from '../drizzle/drizzle.module';
 import { DrizzleDB } from 'src/drizzle/types/drizzle';
 import { asc, desc, eq, InferInsertModel } from 'drizzle-orm';
@@ -10,133 +17,145 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { ConfigService } from '@nestjs/config';
 import { LoginUserDto } from './dto/login-user.dto';
 import { User } from 'src/types';
-import { Response } from 'express';
 
 @Injectable()
 export class UserService {
-    private readonly jwtSecret: string
+  private readonly jwtSecret: string;
 
-    constructor(
-        @Inject(DRIZZLE)
-        private db: DrizzleDB,
-        private configService: ConfigService,
-    ) {
-        this.jwtSecret = this.configService.getOrThrow("JWT_SECRET");
+  constructor(
+    @Inject(DRIZZLE)
+    private db: DrizzleDB,
+    private configService: ConfigService,
+  ) {
+    this.jwtSecret = this.configService.getOrThrow('JWT_SECRET');
+  }
+
+  findAll(
+    limit: number,
+    page: number,
+    orderBy: string,
+    order: string,
+  ): Promise<User[]> {
+    const orderFunc = order === 'asc' ? asc : desc;
+    const orderByParam = ['id', 'name', 'email'].includes(orderBy)
+      ? users[orderBy]
+      : users.id;
+    return this.db.query.users.findMany({
+      limit,
+      offset: page * limit,
+      orderBy: [orderFunc(orderByParam)],
+      columns: {
+        password: false,
+      },
+    });
+  }
+
+  async getUserFromToken(token: string) {
+    try {
+      const decoded = jwt.verify(token, this.jwtSecret) as { id: number };
+
+      if (!decoded || !decoded.id) {
+        throw new UnauthorizedException('Invalid token');
+      }
+
+      return this.findOne(decoded.id);
+    } catch (error) {
+      console.log(error);
+      throw new UnauthorizedException('Invalid or expired token');
+    }
+  }
+
+  async findOne(id: number): Promise<User> {
+    const user = await this.db.query.users.findFirst({
+      where: eq(users.id, id),
+      columns: {
+        password: false,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException(`User with id: ${id} not found`);
     }
 
-    findAll(limit: number, page: number, orderBy: string, order: string): Promise<User[]> {
-        const orderFunc = order === "asc" ? asc : desc;
-        const orderByParam = ["id", "name", "email"].includes(orderBy) ? users[orderBy] : users.id;
+    return user;
+  }
 
-        return this.db.query.users.findMany({
-            limit,
-            offset: page*limit,
-            orderBy: [orderFunc(orderByParam)],
-            columns: {
-                password: false,
-            },
-        });
+  async createUser(data: CreateUserDto): Promise<string> {
+    const hashPassword = await bcrypt.hash(data.password, 10);
+
+    const user = await this.db.query.users.findFirst({
+      where: eq(users.email, data.email),
+      columns: {
+        password: false,
+      },
+    });
+
+    if (user) {
+      throw new ConflictException('User with this email already exists');
     }
 
-    async getUserFromToken(res: Response) {
-        try {
-            const response = res.getHeader('Authorization');
-            const token = response.split(' ')[1];
-            const decoded = jwt.verify(token, this.jwtSecret) as { id: number};
+    const newUser = await this.db
+      .insert(users)
+      .values({
+        name: data.name,
+        email: data.email,
+        password: hashPassword,
+      })
+      .returning({ id: users.id, name: users.name, email: users.email })[0];
+    return jwt.sign({ id: newUser.id }, this.jwtSecret, { expiresIn: '30d' });
+  }
 
-            if (!decoded || !decoded.id) {
-                throw new UnauthorizedException('Invalid token');
-            }
+  async updateUser(id: number, data: UpdateUserDto): Promise<User> {
+    const user = await this.db.query.users.findFirst({
+      where: eq(users.id, id),
+    });
 
-            return this.findOne(decoded.id);
-        } catch(error) {
-            throw new UnauthorizedException('Invalid or expired token');
-        }
+    if (!user) {
+      throw new NotFoundException(`User with id: ${id} not found`);
+    }
+    if (!data.name && !(data.oldPassword && data.password)) {
+      throw new BadRequestException('No data to update');
     }
 
-    async findOne(id: number): Promise<User> {
-        const user = await this.db.query.users.findFirst({
-            where: eq(users.id, id),
-            columns: {
-                password: false,
-            },
-        });
+    const updateData: Partial<InferInsertModel<typeof users>> = {};
+    if (data.name) updateData.name = data.name;
 
-        if (!user) {
-            throw new NotFoundException(`User with id: ${id} not found`)
-        }
-
-        return user;
+    if (data.password && data.oldPassword) {
+      const passwordMatch = await bcrypt.compare(
+        data.oldPassword,
+        user.password,
+      );
+      if (!passwordMatch) {
+        throw new BadRequestException("Passwords don't match");
+      }
+      const newHashPassword = await bcrypt.hash(data.password, 10);
+      updateData.password = newHashPassword;
     }
 
-    async createUser(data: CreateUserDto): Promise<string> {
-        const hashPassword = await bcrypt.hash(data.password, 10)
+    return await this.db
+      .update(users)
+      .set({
+        ...updateData,
+      })
+      .where(eq(users.id, id))
+      .returning({ id: users.id, name: users.name, email: users.email })[0];
+  }
 
-        const user = await this.db.query.users.findFirst({
-            where: eq(users.email, data.email),
-            columns: {
-                password: false,
-            },
-        });
-
-        if (user) {
-            throw new ConflictException('User with this email already exists');
-        }
-
-
-        const newUser = await this.db.insert(users).values({
-            name: data.name,
-            email: data.email,
-            password: hashPassword,
-        }).returning({ id: users.id, name: users.name, email: users.email })[0];
-        
-        return jwt.sign({ id: newUser.id }, this.jwtSecret, { expiresIn: '30d' });
+  async loginUser(data: LoginUserDto): Promise<string> {
+    const user = await this.db.query.users.findFirst({
+      where: eq(users.email, data.email),
+    });
+    console.log(user);
+    if (!user) {
+      throw new NotFoundException(`User with email ${data.email} not found`);
     }
 
-    async updateUser(id: number, data: UpdateUserDto): Promise<User> {
-        const user = await this.db.query.users.findFirst({
-            where: eq(users.id, id),
-        });
-        
-        if (!user) {
-            throw new NotFoundException(`User with id: ${id} not found`);
-        }
-        if (!data.name && !(data.oldPassword && data.password)) {
-            throw new BadRequestException('No data to update');
-        }
+    const passwordMatch = await bcrypt.compare(data.password, user.password);
 
-        const updateData: Partial<InferInsertModel<typeof users>> = {};
-        if(data.name) updateData.name = data.name;
-
-        if (data.password && data.oldPassword) {
-            const passwordMatch = await bcrypt.compare(data.oldPassword, user.password);
-            if(!passwordMatch) {
-                throw new BadRequestException("Passwords don't match");
-            }
-            const newHashPassword = await bcrypt.hash(data.password, 10);
-            updateData.password = newHashPassword;
-        }
-        
-        return await this.db.update(users).set({
-        ...updateData
-        }).where(eq(users.id, id)).returning({ id: users.id, name: users.name, email: users.email })[0];
+    if (!passwordMatch) {
+      throw new BadRequestException("Passwords don't match");
     }
 
-    async loginUser(data: LoginUserDto): Promise<string> {
-        const user = await this.db.query.users.findFirst({
-            where: eq(users.email, data.email)
-        });
-
-        if (!user) {
-            throw new NotFoundException(`User with email ${data.email} not found`);
-        }
-
-        const passwordMatch = await bcrypt.compare(data.password, user.password);
-
-        if (!passwordMatch) {
-            throw new BadRequestException("Passwords don't match");
-        }
-        
-        return jwt.sign({ id: user.id }, this.jwtSecret, { expiresIn: '30d' });
-    }
+    return jwt.sign({ id: user.id }, this.jwtSecret, { expiresIn: '30d' });
+  }
 }
